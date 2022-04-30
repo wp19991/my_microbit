8a5e549acfb2b1965778d926cfc4570638d5b511
参考答案开心否 <46703189+wp19991@users.noreply.github.com>
2022-05-01 00:13:06 +0800

    更新 main.py

--- main.py
+++ main.py
@@ -9,6 +9,6 @@ def on_button_pressed_b():
     for i in a_list:
         basic.show_number(i)
     basic.clear_screen()
-    
+
 input.on_button_pressed(Button.A, on_button_pressed_a)
 input.on_button_pressed(Button.B, on_button_pressed_b)
